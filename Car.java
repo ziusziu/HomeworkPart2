3e829315e6733e6115e46babb0b5acdd601eb5a8
Michael B <michaelbor2015@gmail.com>
2016-02-22 18:56:13 -0800

    Part2

---
+++ Car.java
@@ -0,0 +1,17 @@
+/**
+ * Creator: Sam Siu
+ * Date: 02/22/2016
+ */
+
+public class Car extends Main{
+
+        Car 1 = new Car;
+        Car 2 = new Car;
+        Car 3 = new Car;
+    
+        public static void main(String[] args){
+            
+          
+
+    
+}
---
+++ Main.java
@@ -0,0 +1,11 @@
+/**
+ * Creator: Sam Siu
+ * Date: 02/22/2016
+ */
+
+public class Git {
+    public static void main(String[] args){
+
+    }
+
+}
